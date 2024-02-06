f0c5f02bae29943626d6b46d519b533d36487411
Tomás <tomasarreguib@gmail.com>
2024-02-05 23:09:11 -0300

    test: defined unit tests for SellerService

---
+++ src/seller/seller.service.spec.ts
@@ -0,0 +1,69 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { SellerService } from './seller.service';
+import { Seller } from '../db/entities/seller';
+import { Role } from '../types';
+
+describe('SellerService', () => {
+  let service: SellerService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SellerService],
+    }).compile();
+
+    service = module.get<SellerService>(SellerService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should has defined createSeller method', () => {
+    expect(service.createSeller).toBeDefined();
+  });
+
+  it('should add the new seller to the array of sellers', () => {
+    const sellers = service.sellers;
+    const newSeller = {
+      id: 4,
+      firstName: 'Foo',
+      lastName: 'Test',
+      mail: 'footest@gmail.com',
+      role: Role.SELLER,
+      password: 'strongpassword',
+    };
+
+    service.createSeller(newSeller);
+
+    /*
+      testing the new seller is added to the end of the array
+    */
+    expect(sellers[sellers.length - 1]).toStrictEqual<Seller>({
+      id: 4,
+      firstName: 'Foo',
+      lastName: 'Test',
+      mail: 'footest@gmail.com',
+      role: Role.SELLER,
+      password: 'strongpassword',
+    });
+  });
+
+  it('should has defined getSellers method', () => {
+    expect(service.getSellers).toBeDefined();
+  });
+
+  it('should return an array of all registered sellers', () => {
+    const allSellers = service.getSellers();
+
+    expect(allSellers).toBeInstanceOf(Array<Seller>);
+    expect(allSellers[0]).toStrictEqual<Seller>({
+      id: 1,
+      firstName: 'Chris',
+      lastName: 'Gomm',
+      mail: 'chrimsdummy@gmail.com',
+      password: 'test123',
+      role: Role.SELLER,
+    });
+  });
+});
